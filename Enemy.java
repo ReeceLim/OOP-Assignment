70925d2123089a88e73ba88f935e61f75607fb80
ReeceLim <c250215@e.ntu.edu.sg>
2026-03-30 16:12:54 +0800

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,6 +1,11 @@
-public class Enemy {
+public class Enemy 
+{
     private int HP;
     private int dmg;
 
-    
+    public void Atk(Enemy enemy, int Atk)
+    {
+        enemy.setHP();
+    }
+
 }
