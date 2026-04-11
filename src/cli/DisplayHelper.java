b2b4e023caa4cfc9faac7eb22d8f000a9d6caebc
kira384 <87072847+kira384@users.noreply.github.com>
2026-04-11 16:06:11 +0800

    displayhelper.java updated

--- src/cli/DisplayHelper.java
+++ src/cli/DisplayHelper.java
@@ -1,1 +1,26 @@
+package cli;
 
+import base.PlayerBase;
+
+public class DisplayHelper {
+    public void showWelcome() {
+        System.out.println("==================================");
+        System.out.println("     TURN-BASED COMBAT GAME");
+        System.out.println("==================================");
+    }
+
+    public void showClassMenu() {
+        System.out.println("\nChoose your class:");
+        System.out.println("1. Warrior");
+        System.out.println("2. Wizard");
+    }
+
+    public void showPlayerInfo(PlayerBase player) {
+        System.out.println("\n===== PLAYER INFO =====");
+        System.out.println("Name: " + player.getName());
+        System.out.println("HP: " + player.getCurrentHp() + "/" + player.getMaxHp()); //get the "currentHp out of maxHp"
+        System.out.println("ATK: " + player.getAttack());
+        System.out.println("DEF: " + player.getDefense());
+        System.out.println("SPD: " + player.getSpeed());
+    }
+}
